329934fc9b7b5b148729b5d13767480dfec05e3b
吴佳辉 <17333791213@163.com>
2025-02-07 14:27:17 +0800

    2

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,17 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  webpack(config, { isServer }) {
+    if (isServer) {
+      config.plugins.push(
+        // eslint-disable-next-line @typescript-eslint/no-require-imports
+        new (require('webpack')).IgnorePlugin({
+          resourceRegExp: /next-font-manifest\.json/,
+        })
+      );
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
